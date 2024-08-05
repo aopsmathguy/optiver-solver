25854bf40ab742b2212429fec07287fe80072cb6
Min Ding <min@Taos-MacBook-Pro.local>
2024-08-05 15:24:09 -0400

    test

--- frontend/src/ws/index.js
+++ frontend/src/ws/index.js
@@ -2,7 +2,7 @@
 import { SocketClient } from '../common/socketUtility';
 import { CONFIG } from '../common/socketConfig';
 
-const WEBSOCKET_HOST = "ws://localhost:3000/api/ws";
+const WEBSOCKET_HOST = "ws://figure-it-out.onrender.com/api/ws";
 export const socket = new SocketClient(null, CONFIG);
 function connect(){
     socket.ws = new WebSocket(WEBSOCKET_HOST);
